1abc3bc3376f4e06e34bd78639f195955c76d7b1
Aman Garg <amangarg@Amans-MacBook-Air.local>
2025-02-17 13:25:21 +0530

    first commit

--- app.py
+++ app.py
@@ -286,7 +286,7 @@ def query_endpoint():
         )
         for chunk in explanation_response:
             if chunk.choices[0].delta.content is not None:
-               yield  json.dumps({"response": chunk.choices[0].delta.content})
+               yield f"data: {json.dumps({'response': chunk.choices[0].delta.content})}\n\n"
 
         prompt_documents = ""
         for i, doc in enumerate(final_docs, start=1):
@@ -360,8 +360,8 @@ def query_endpoint():
         )
         for chunk in reference_response:
             if chunk.choices[0].delta.content is not None:
-                yield  json.dumps({"response": chunk.choices[0].delta.content})
-    return Response(generate(),content_type="application/x-ndjson")
+                yield f"data: {json.dumps({'response': chunk.choices[0].delta.content})}\n\n"
+    return Response(stream_with_context(generate()), content_type="text/event-stream")
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
